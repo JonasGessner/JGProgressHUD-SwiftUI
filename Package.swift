69898e42da98bc08076bbf158e14d2aeec4db78e
Jonas Gessner <support@j-gessner.de>
2020-10-15 17:29:32 +0200

    Finalize Package.swift

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/JonasGessner/JGProgressHUD.git", .branch("master")),
+        .package(url: "https://github.com/JonasGessner/JGProgressHUD.git", from: "2.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JGProgressHUD-SwiftUI",
-            dependencies: [.product(name: "JGProgressHUD", package: "JGProgressHUD")]),
+            dependencies: [.product(name: "JGProgressHUD", package: "JGProgressHUD")]
+            )
     ]
 )
-
